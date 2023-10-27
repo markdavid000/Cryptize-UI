8a51ad57b22204305acc1a1b23b58dd8ff8e29c3
Mark-David Ojukwu <davidojukwu59@gmail.com>
2023-10-27 18:54:52 +0100

    cryptize_continuation

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -5,14 +5,28 @@ import { CTABtn } from "./CTABtn";
 
 const CTA = () => {
   return (
-    <Box mt={'9rem'}  bgGradient="linear(to-tr, #A55EFE, #724BFD, #3547F1, #3547F1, #2C79D5, #2C79D5)">
-      <Box maxW={"1150px"} mx={"auto"} px={'50px'} py={'70px'}>
-        <Flex alignItems={"center"}>
-          <Box>
+    <Box
+      mt={"9rem"}
+      bgGradient="linear(to-tr, #A55EFE, #724BFD, #3547F1, #3547F1, #2C79D5, #2C79D5)"
+    >
+      <Box
+        maxW={"1150px"}
+        mx={"auto"}
+        px={["20px", "20px", "50px"]}
+        py={"70px"}
+      >
+        <Flex alignItems={"center"} flexDirection={["column", "column", "row"]}>
+          <Box textAlign={["center", "center", "left"]}>
             <SectionHeading>
               Tools For Better Crypto Trading & Transactions
             </SectionHeading>
-            <Text w={'47%'} py={'1em'} color={"white"} fontSize={"14px"} fontWeight={"400"}>
+            <Text
+              w={["100%", "100%", "47%"]}
+              py={"1em"}
+              color={"white"}
+              fontSize={"14px"}
+              fontWeight={"400"}
+            >
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
               eget nulla bibendum, semper nisi sed, volutpat massa.{" "}
             </Text>
